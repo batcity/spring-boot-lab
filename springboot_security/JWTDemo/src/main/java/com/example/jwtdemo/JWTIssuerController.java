0365a1a0ea345dcdf6775bb0b5f7bf70ad19db42
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-12-25 02:49:01 +0530

    refine jwt demo

--- springboot_security/JWTDemo/src/main/java/com/example/jwtdemo/JWTIssuerController.java
+++ springboot_security/JWTDemo/src/main/java/com/example/jwtdemo/JWTIssuerController.java
@@ -2,6 +2,15 @@ package com.example.jwtdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,9 +22,27 @@ class JWTIssuerController {
         SpringApplication.run(JWTIssuerController.class, args);
     }
 
+    // 🔐 Demo secret key (DO NOT hardcode in real apps)
+    private static final Key SIGNING_KEY =
+            Keys.hmacShaKeyFor(
+                    "super-secret-signing-key-longer-than-256-bits"
+                            .getBytes()
+            );
 
-    @GetMapping
+
+    @GetMapping("/login")
     public String login() {
-        return "stub";
+
+        Instant now = Instant.now();
+
+        String jwt = Jwts.builder()
+                .setSubject("demo-user")
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(now.plusSeconds(3600))) // 1 hour
+                .claim("role", "USER")
+                .signWith(SIGNING_KEY, SignatureAlgorithm.HS256)
+                .compact();
+
+        return jwt;
     }
 }
---
+++ springboot_security/JWTDemo/src/main/java/com/example/jwtdemo/JWTValidatorController.java
@@ -0,0 +1,31 @@
+package com.example.jwtdemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
+@SpringBootApplication
+@RestController
+class JWTValidatorController {
+
+    // 🔐 Demo secret key (DO NOT hardcode in real apps)
+    private static final Key SIGNING_KEY =
+            Keys.hmacShaKeyFor(
+                    "super-secret-signing-key-longer-than-256-bits"
+                            .getBytes()
+            );
+
+
+    @GetMapping("/auth/validate")
+    public String validate(@RequestHeader(value = "Authorization", required = false) String authHeader) {
+            if (authHeader == null) {
+                return "No token";
+            }
+            return "Has token";
+    }
+}
